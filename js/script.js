6cae894074637dbb69033b74153a8960c3b9d336
Vida <134993237+VidaWee@users.noreply.github.com>
2023-07-06 19:57:54 +1200

    Accept & Validate Player Guesses

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const displayGuessSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again hide")
 const word = "magnolia";
+const AllGuessedLetters = [];
 
 const dummyText = function (word){
     const placeholderLetters = [];
@@ -22,8 +23,42 @@ dummyText(word);
 guessButton.addEventListener("click", function(e){
 
     e.preventDefault();
+    message.innerText = "";
     const letterInput = textInput.value;
-    console.log(letterInput);
-    letterInput.input = "";
+    const goodGuess = validateInput(letterInput);
+    textInput.value = "";
 
+    if (goodGuess) {
+    
+      makeGuess(letterInput);
+    }
+    letterInput.value = "";
 });
+
+
+const validateInput = function(input){
+  const acceptedLetter = /[a-zA-Z]/
+  if (input.length === 0){
+    message.innerText = "Please enter a letter.";
+  } else if (input.length>1){
+    message.innerText = "Please enter a single letter.";    
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z.";
+  }else{
+  return input;
+  }
+};
+
+const makeGuess = function(guess){
+
+  guess=guess.toUpperCase();
+
+  if(AllGuessedLetters.includes(guess)){
+
+    message.innerText ="Already guessed the letter please try again!";
+  }else{
+    AllGuessedLetters.push(guess)
+    console.log(AllGuessedLetters);
+  }
+
+};
